4c89f52ccce3ab84e8e43e74f01676285d83a229
Psidave <aorig111@gmail.com>
2025-05-03 12:00:13 +0800

    psyduck

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from '../stylesheet/home.module.css';
 
+
 const Home = () => {
   return (
     <div className={styles.home}>
     <div className={styles.content}>
-      <h1 className={styles.centeredHeading}>Welcome to my site.</h1>
+      <h1 className={styles.centeredHeading}>Psyduck</h1>
     </div>
 
   </div>
