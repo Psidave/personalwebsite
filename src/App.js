4817d17840dd38e3e498cd55277b60173a6904e0
Psidave <aorig111@gmail.com>
2025-05-11 19:38:34 +0800

    replace better buttons and more user interface

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 import Home from './pages/home.jsx';
-
-const Button = styled.button`
-  background-color: rgb(255, 255, 255);
-  width: 200px;        /* sets the width */
-  height: 50px;        /* sets the height */
-  font-size: 18px;     /* controls the text size inside */
-  padding: 10px 20px;  /* controls the space inside the button */
-  bottom: 500px;
-  right: 1400px;
-`;
-
-function clickMe() {
-  alert('You clicked me!');
-}
 
 function App() {
   return (
     <div>
       <Home />
-      <Button onClick={clickMe}>
-        My Portfoio
-      </Button>
     </div>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import styles from '../stylesheet/home.module.css';
 
-
 const Home = () => {
+  // Define the function INSIDE the component
+  const handleClick = () => {
+    alert('You clicked me!');
+  };
+
   return (
     <div className={styles.home}>
-    <div className={styles.content}>
-      <h1 className={styles.centeredHeading}>I am Adrian Orig</h1>
+      <div className={styles.content}>
+       <button className={styles.homeButton} onClick={handleClick}> Home </button>
+       <button className={styles.aboutButton} onClick={handleClick}> About Me </button>
+        <h1 className={styles.centeredHeading}>I am Adrian Orig</h1>
+        <button className={styles.portfolioButton} onClick={handleClick}>My Portfolio </button>
+      </div>
     </div>
-
-  </div>
-);
+  );
 };
 
 export default Home;
